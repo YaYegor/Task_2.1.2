ab54d8361e481d2b7fe20446c9222ea67783331b
negrulya <sunohara29@yandex.ru>
2022-09-14 13:04:31 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld hello1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(hello1.getMessage());
+        HelloWorld hello2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(hello1 == hello2);
+        System.out.println(cat1 == cat2);
     }
 }
